d7f3a84cc514714d33c289db77396d7c6ceed300
Henry Okpoti <okpot2000@yahoo.com>
2025-09-28 18:42:33 -0400

    Revert "deleted vpc block"
    This reverts commit ea89e679abd4e9372304f3a62ad38a1e1c9e5452.

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,4 +1,11 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = "default"
 
+tags = merge(var.tags, {
+    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-main-vpc"
+  })
+}
 
 # Creating internet Gateway ------------------------------------------------------------
 resource "aws_internet_gateway" "igw" {
